8780aef3e79bd9df6b0c495530a5c88307d66366
Sidi Becker <sidibecker@hotmail.com>
2022-05-03 22:10:06 -0300

    firebase analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { environment } from 'src/environments/environment.local';
+import {
+  Analytics,
+  getAnalytics,
+  logEvent,
+  setAnalyticsCollectionEnabled,
+} from 'firebase/analytics';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sidi-becker';
+
+  private analytics!: Analytics;
+
+  constructor() {
+    const app = initializeApp(environment.FIREBASE_CONFIG);
+    const analytics = getAnalytics(app);
+
+    this.analytics = analytics;
+
+    setAnalyticsCollectionEnabled(analytics, true);
+  }
+
+  visit(target: string) {
+    logEvent(this.analytics, 'visit', { target });
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  FIREBASE_CONFIG: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
+  },
 };
