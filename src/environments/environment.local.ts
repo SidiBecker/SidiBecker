7cfedb6bbe05652bdf401dbe1f15b150e60dc25b
SidiBecker <sidibecker@hotmail.com>
2022-12-12 14:13:06 -0300

    update

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyDqmg3ThdEs2C8ISzu4deFqwXwj1Pm2-U4",
+    authDomain: "my-website-20b35.firebaseapp.com",
+    projectId: "my-website-20b35",
+    storageBucket: "my-website-20b35.appspot.com",
+    messagingSenderId: "195177022481",
+    appId: "1:195177022481:web:f6b721e7ac30dc66bdddb0",
+    measurementId: "G-EMXNM2RJJZ"
+  },
+};
